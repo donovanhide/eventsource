898b4800dcfae4f4b2420394722ac299fd546de9
Donovan Hide <donovanhide@gmail.com>
2015-03-18 17:49:05 +0000

    Add Close() functionality to Stream
    Strongly based on code from phemmer:
    https://github.com/donovanhide/eventsource/pull/10
    * Added Close() functionality to Stream
    * Added Close() test
    * Don’t block if client doesn’t consume Stream.Errors

--- stream.go
+++ stream.go
@@ -9,6 +9,15 @@ import (
 	"time"
 )
 
+type SubscriptionError struct {
+	Code    int
+	Message string
+}
+
+func (e SubscriptionError) Error() string {
+	return fmt.Sprintf("%d: %s", e.Code, e.Message)
+}
+
 // Stream handles a connection for receiving Server Sent Events.
 // It will try and reconnect if the connection is lost, respecting both
 // received retry delays and event id's.
@@ -23,16 +32,9 @@ type Stream struct {
 	// It's mainly for informative purposes - the client isn't required to take any
 	// action when an error is encountered. The stream will always attempt to continue,
 	// even if that involves reconnecting to the server.
-	Errors chan error
-}
-
-type SubscriptionError struct {
-	Code    int
-	Message string
-}
-
-func (e SubscriptionError) Error() string {
-	return fmt.Sprintf("%d: %s", e.Code, e.Message)
+	Errors  chan error
+	quit    chan bool
+	hasQuit chan bool
 }
 
 // Subscribe to the Events emitted from the specified url.
@@ -43,52 +45,83 @@ func Subscribe(url, lastEventId string) (*Stream, error) {
 		lastEventId: lastEventId,
 		retry:       (time.Millisecond * 3000),
 		Events:      make(chan Event),
-		Errors:      make(chan error),
+		Errors:      make(chan error, 1),
+		quit:        make(chan bool),
+		hasQuit:     make(chan bool),
 	}
 	r, err := stream.connect()
 	if err != nil {
 		return nil, err
 	}
-	go stream.stream(r)
+	go stream.monitor(r)
 	return stream, nil
 }
 
-func (stream *Stream) connect() (r io.ReadCloser, err error) {
-	var resp *http.Response
-	var req *http.Request
-	if req, err = http.NewRequest("GET", stream.url, nil); err != nil {
-		return
+// Close will stop the stream from reading any further events from the server.
+// Not safe to be called by more than one goroutine.
+func (stream *Stream) Close() {
+	stream.quit <- true
+	<-stream.hasQuit
+}
+
+func (stream *Stream) connect() (io.ReadCloser, error) {
+	req, err := http.NewRequest("GET", stream.url, nil)
+	if err != nil {
+		return nil, err
 	}
 	req.Header.Set("Cache-Control", "no-cache")
 	req.Header.Set("Accept", "text/event-stream")
 	if len(stream.lastEventId) > 0 {
 		req.Header.Set("Last-Event-ID", stream.lastEventId)
 	}
-	if resp, err = stream.c.Do(req); err != nil {
-		return
+	resp, err := stream.c.Do(req)
+	if err != nil {
+		return nil, err
 	}
 	if resp.StatusCode != 200 {
 		message, _ := ioutil.ReadAll(resp.Body)
-		err = SubscriptionError{
+		return nil, SubscriptionError{
 			Code:    resp.StatusCode,
 			Message: string(message),
 		}
 	}
-	r = resp.Body
-	return
+	return resp.Body, nil
 }
 
-func (stream *Stream) stream(r io.ReadCloser) {
-	defer r.Close()
-	dec := newDecoder(r)
+func (stream *Stream) monitor(r io.ReadCloser) {
+	e := make(chan error)
+	go stream.stream(e, r)
 	for {
-		ev, err := dec.Decode()
-
-		if err != nil {
-			stream.Errors <- err
-			// respond to all errors by reconnecting and trying again
-			break
+		select {
+		case err := <-e:
+			r.Close()
+			stream.sendError(err)
+		case <-stream.quit:
+			r.Close()
+			<-e
+			close(stream.Errors)
+			close(stream.Events)
+			stream.hasQuit <- true
+			return
 		}
+		for backoff := stream.retry; ; backoff *= 2 {
+			log.Printf("Reconnecting in %0.4f secs", backoff.Seconds())
+			var err error
+			r, err = stream.connect()
+			if err == nil {
+				go stream.stream(e, r)
+				break
+			}
+			stream.sendError(err)
+			time.Sleep(backoff)
+		}
+	}
+}
+
+func (stream *Stream) stream(e chan error, r io.ReadCloser) {
+	dec := newDecoder(r)
+	ev, err := dec.Decode()
+	for ; err == nil; ev, err = dec.Decode() {
 		pub := ev.(*publication)
 		if pub.Retry() > 0 {
 			stream.retry = time.Duration(pub.Retry()) * time.Millisecond
@@ -98,20 +131,13 @@ func (stream *Stream) stream(r io.ReadCloser) {
 		}
 		stream.Events <- ev
 	}
-	backoff := stream.retry
-	for {
-		time.Sleep(backoff)
-		log.Printf("Reconnecting in %0.4f secs", backoff.Seconds())
+	e <- err
+}
 
-		// NOTE: because of the defer we're opening the new connection
-		// before closing the old one. Shouldn't be a problem in practice,
-		// but something to be aware of.
-		next, err := stream.connect()
-		if err == nil {
-			go stream.stream(next)
-			break
-		}
-		stream.Errors <- err
-		backoff *= 2
+func (stream *Stream) sendError(err error) {
+	select {
+	case stream.Errors <- err:
+	default:
+		// consumer is apparently ignoring errors
 	}
 }
