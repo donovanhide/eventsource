b25d408bd0254fa3fb80d1579ed688b94e40e9c1
John Kodumal <john@launchdarkly.com>
2015-07-17 10:25:56 -0700

    Return the stream even if subscribing fails

--- stream.go
+++ stream.go
@@ -55,11 +55,9 @@ func Subscribe(url string, headers http.Header, lastEventId string) (*Stream, er
 		c:           &http.Client{},
 	}
 	r, err := stream.connect()
-	if err != nil {
-		return nil, err
-	}
+
 	go stream.stream(r)
-	return stream, nil
+	return stream, err
 }
 
 func copyHeader(dst, src http.Header) {
