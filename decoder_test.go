50de6daa2091e3768e0ebab85341be256158cd6d
Marco Jantke <marco.jantke@gmail.com>
2017-06-30 06:26:07 +0200

    improve tests for decoding events

--- decoder_test.go
+++ decoder_test.go
@@ -1,6 +1,7 @@
 package eventsource
 
 import (
+	"io"
 	"reflect"
 	"strings"
 	"testing"
@@ -8,29 +9,39 @@ import (
 
 func TestDecode(t *testing.T) {
 	tests := []struct {
-		rawInput    string
-		wantedEvent *publication
+		rawInput     string
+		wantedEvents []*publication
 	}{
 		{
-			rawInput:    "event: eventName\ndata: {\"sample\":\"value\"}\n\n",
-			wantedEvent: &publication{event: "eventName", data: "{\"sample\":\"value\"}"},
+			rawInput:     "event: eventName\ndata: {\"sample\":\"value\"}\n\n",
+			wantedEvents: []*publication{{event: "eventName", data: "{\"sample\":\"value\"}"}},
 		},
 		{
 			// the newlines should not be parsed as empty event
-			rawInput:    "\n\n\nevent: eventName\n\n",
-			wantedEvent: &publication{event: "eventName"},
+			rawInput:     "\n\n\nevent: event1\n\n\n\n\nevent: event2\n\n",
+			wantedEvents: []*publication{{event: "event1"}, {event: "event2"}},
 		},
 	}
 
 	for _, test := range tests {
 		decoder := NewDecoder(strings.NewReader(test.rawInput))
-		event, err := decoder.Decode()
-		if err != nil {
-			t.Fatalf("Unexpected error on decoding event: %s", err)
-		}
+		i := 0
+		for {
+			event, err := decoder.Decode()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				t.Fatalf("Unexpected error on decoding event: %s", err)
+			}
 
-		if !reflect.DeepEqual(event, test.wantedEvent) {
-			t.Errorf("Parsed event %+v does not equal wanted event %+v", event, test.wantedEvent)
+			if !reflect.DeepEqual(event, test.wantedEvents[i]) {
+				t.Fatalf("Parsed event %+v does not equal wanted event %+v", event, test.wantedEvents[i])
+			}
+			i++
+		}
+		if i != len(test.wantedEvents) {
+			t.Fatalf("Unexpected number of events: %d does not equal wanted: %d", i, len(test.wantedEvents))
 		}
 	}
 }
